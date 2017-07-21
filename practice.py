9d8bdf1fbac4ad4d0085765601828588dbca78bc
Gia Reyes <giareyes2005@gmail.com>
2017-07-20 23:23:03 -0400

    someone pls take me out of my misery (#5)
    bless u michelle

--- practice.py
+++ practice.py
@@ -1,4 +1,8 @@
+from random import *
+import datetime
+
 def print_date():
-    print(datetime.now())
-    print_random_number()
-    print_random_number()
+    print(datetime.datetime.now())
+
+def print_random_number():
+    print(random())
